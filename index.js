3c2f5ff2aa7c8b15649510dffe8e300e522a0023
Dhoni-creator <dhonihadwo@gmail.com>
2025-02-27 15:02:47 +0700

    new portofolio

---
+++ index.js
@@ -0,0 +1,12 @@
+function add1(){
+    document.querySelector('.c1').classList.toggle('show')
+}
+function add2(){
+    document.querySelector('.c2').classList.toggle('show')
+}
+function add3(){
+    document.querySelector('.c3').classList.toggle('show')
+}
+function add4(){
+    document.querySelector('.c4').classList.toggle('show')
+}
