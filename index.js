74343a385a6b822f4ddf63cb12b8fbebe6a86151
Dhoni-creator <dhonihadwo@gmail.com>
2025-03-01 19:05:47 +0700

    hai

--- index.js
+++ index.js
@@ -10,3 +10,29 @@ function add3(){
 function add4(){
     document.querySelector('.c4').classList.toggle('show')
 }
+
+window.addEventListener('scroll', () => {
+    let scroll = window.scrollY
+    console.log(scroll)
+
+    if(scroll < 778.4000244140625){
+        document.querySelector('.active').classList.remove('active')
+        document.querySelector('.home').classList.add('active')
+    }
+    else if(scroll < 1507.199951171875){
+        document.querySelector('.active').classList.remove('active')
+        document.querySelector('.about').classList.add('active')
+    }
+    else if(scroll < 2908){
+        document.querySelector('.active').classList.remove('active')
+        document.querySelector('.skill').classList.add('active')
+    }
+    else if(scroll < 3448){
+        document.querySelector('.active').classList.remove('active')
+        document.querySelector('.project').classList.add('active')
+    }
+    else if(scroll = 3448){
+        document.querySelector('.active').classList.remove('active')
+        document.querySelector('.contact').classList.add('active')
+    }
+})
